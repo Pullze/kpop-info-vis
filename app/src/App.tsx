d83d9b770edc8890e207fd9717052b3cdb8257d1
Lai Wang <lai1007@outlook.com>
2024-11-28 15:28:19 -0500

    update style

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -13,7 +13,20 @@ function App() {
   }, []);
 
   return (
-    <Article content={htmlContent} />
+    <>
+      <div className={"w-full h-80 bg-neutral-900"} />
+      <div
+        className={"prose dark:prose-invert prose-sm mx-auto my-8 px-4 md:my-16 md:px-8 md:prose-base lg:prose-lg xl:max-w-screen-lg"}>
+        <h1 className="not-prose text-neutral-950 scroll-m-20 text-4xl md:text-6xl font-bold leading-loose lg:text-7xl">
+          K-pop Invasion: From a Local Trend to a Global Phenomenon
+        </h1>
+        <p className={"uppercase text-base font-semibold"}>By who, and who <span className="ml-8 font-normal">December 1, 2024</span></p>
+      </div>
+      <Article content={htmlContent}/>
+      <div className={"w-full h-80 bg-neutral-900 content-center"}>
+        <h4 className={"text-center text-neutral-400 scroll-m-20 font-medium tracking-tight"}>SI-649 Final Project</h4>
+      </div>
+    </>
   )
 }
 
--- app/src/components/article.tsx
+++ app/src/components/article.tsx
@@ -12,7 +12,7 @@ export default React.forwardRef<
     <article
       ref={ref}
       className={cn(
-        "prose dark:prose-invert prose-sm dark mx-auto my-8 px-4 md:my-16 md:px-8 md:prose-base lg:prose-lg xl:container",
+        "prose dark:prose-invert prose-sm mx-auto my-8 px-4 md:my-16 md:px-8 md:prose-base lg:prose-lg xl:max-w-screen-lg",
         className,
       )}
       dangerouslySetInnerHTML={{ __html: content }}
