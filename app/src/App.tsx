c422f81d7dbbb5028d0640b9c34332f9f6ad744f
Lai Wang <lai1007@outlook.com>
2024-11-29 13:53:40 -0500

    update title style

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <div
           className={"prose dark:prose-invert prose-sm mx-auto my-8 px-4 md:my-16 md:px-8 md:prose-base lg:prose-lg xl:max-w-screen-lg"}>
           <ModeToggle />
-          <h1 className="not-prose dark:text-neutral-50 text-neutral-950 scroll-m-20 text-4xl md:text-6xl font-bold leading-loose lg:text-7xl">
+          <h1 className="not-prose dark:text-neutral-50 text-neutral-950 scroll-m-20 text-4xl md:text-6xl font-bold leading-normal lg:text-7xl">
             K-pop Invasion: From a Local Trend to a Global Phenomenon
           </h1>
           <p className={"uppercase text-base font-semibold"}>By who, and who <span className="ml-8 font-normal">December 1, 2024</span></p>
