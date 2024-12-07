8bbe60b306693c57aeb1caff8d530fd5044b5f52
Lai Wang <lai@gatech.edu>
2024-12-06 21:06:50 -0500

    fix website width and add favicon

--- app/src/components/article.tsx
+++ app/src/components/article.tsx
@@ -9,7 +9,7 @@ export default React.forwardRef<
     <article
       ref={ref}
       className={cn(
-        "prose dark:prose-invert prose-base mx-auto my-8 px-4 md:my-16  lg:prose-lg xl:max-w-screen-lg",
+        "prose dark:prose-invert prose-base mx-auto my-8 px-4 md:my-16 lg:prose-lg xl:max-w-screen-lg",
         className,
       )}
       {...props}
