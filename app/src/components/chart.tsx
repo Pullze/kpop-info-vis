72492481f4d95a93869fdfe0fa327d3a8d0b1c09
Lai Wang <lai@gatech.edu>
2024-12-08 02:23:10 -0500

    add second chart

--- app/src/components/chart.tsx
+++ app/src/components/chart.tsx
@@ -1,20 +1,22 @@
-import {ComponentPropsWithoutRef, useCallback, useEffect, useMemo, useRef, useState} from "react";
+import {ComponentPropsWithoutRef, useCallback, useEffect, useRef, useState} from "react";
 import {cn} from "@/lib/utils.ts";
 import embed from "vega-embed";
+import {useTheme} from "@/components/theme-provider.tsx";
 
 type ChartProps = {
-  spec: string;
+  spec: object;
 } & ComponentPropsWithoutRef<'div'>;
 
 export default function Chart(props: ChartProps) {
   const {spec, className, ...rest} = props;
   const ref = useRef<HTMLDivElement | null>(null);
 
-  const parsedJSON = useMemo(() => JSON.parse(spec), [spec]);
+  const parsedJSON = spec;
   const [width, setWidth] = useState<number>(0);
-  
+  const {theme} = useTheme();
+
   const handleResize = useCallback(() => {
-     setWidth(ref.current?.offsetWidth ?? 200);
+    setWidth(ref.current?.offsetWidth ?? 200);
   }, [setWidth]);
 
   useEffect(() => {
@@ -29,14 +31,20 @@ export default function Chart(props: ChartProps) {
     const divElement = ref.current as HTMLDivElement;
     let cleanUp;
 
-    embed(divElement, parsedJSON, {actions: false, width: width / 2, height: 400}).then((res) => {
+    embed(divElement, parsedJSON, {
+      actions: false,
+      width: width - 200,
+      height: 400,
+      theme: theme === 'dark' ? 'carbong90' : "googlecharts",
+      config: {background: theme === 'dark' ? '#0a0a0a' : "white"}
+    }).then((res) => {
       cleanUp = res.view.finalize;
     });
 
     return cleanUp;
-  }, [parsedJSON, width]);
+  }, [parsedJSON, theme, width]);
 
   return (
-    <div className={cn("w-full not-prose mt-16", className)} {...rest} ref={ref} />
+    <div className={cn("w-full not-prose mt-16", className)} {...rest} ref={ref}/>
   )
 }
